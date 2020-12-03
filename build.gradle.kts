5d1a9789de02c56a84c86e13b16de01926e10825
XuaTheGrate <17090652+XuaTheGrate@users.noreply.github.com>
2020-12-03 19:31:46 +1300

    optimize day stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,15 +8,18 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation("io.ktor", "ktor-client-cio-jvm", "1.4.0")
 }
 
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.freeCompilerArgs += "-Xopt-in=io.ktor.util.KtorExperimentalAPI"
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
--- src/main/kotlin/org/example/aocutil/Day.kt
+++ src/main/kotlin/org/example/aocutil/Day.kt
@@ -1,7 +1,32 @@
 package org.example.aocutil
 
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import java.nio.charset.Charset
+
 interface Day {
-    fun main()
+    fun getHttpEngine(): HttpClient {
+        return HttpClient(CIO)
+    }
+
+    suspend fun collect(day: Int): String {
+        val resp = getHttpEngine().get<HttpResponse>("https://adventofcode.com/2020/day/$day/input") {
+            header("Cookie", "session=${System.getenv("AOC_COOKIE")}")
+        }
+        if (resp.status.value != 200) {
+            throw RuntimeException("Invalid response code: ${resp.status.value} ${resp.status.description}")
+        }
+        return resp.readText(Charset.forName("UTF-8"))
+    }
+
+    fun preprocess()
+    fun main() {
+        preprocess()
+        partOne()
+        partTwo()
+    }
     fun partOne()
     fun partTwo()
 }
